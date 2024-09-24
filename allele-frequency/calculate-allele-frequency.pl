4e4d8cb45c4c4563bd5ca9483210979f79c2bb9b
maurya-anand <anandmaurya@hotmail.com>
2024-09-24 13:55:14 +0100

    added: command-line options (--vcf and --out) to specify the input VCF file and the output file respectively.

--- allele-frequency/calculate-allele-frequency.pl
+++ allele-frequency/calculate-allele-frequency.pl
@@ -1,13 +1,22 @@
 #!/usr/bin/env perl
 
 # script to calculate allele frequency from a multi sample VCF file
-# usage: perl calculate-allele-frequency.pl example/genotypes.vcf > example/allele_frequencies.tsv
+# usage: perl calculate-allele-frequency.pl --vcf example/genotypes.vcf --out example/allele_frequencies.tsv
 
 use strict;
 use warnings;
-my $inp_vcf = $ARGV[0]; # input VCF file
-open(VCF, "$inp_vcf") or die "can't open the VCF file";
-print "CHR\tPOS\tREF\tALT\tALT_ALLELE_FREQ\tMINOR_ALLELE\tMINOR_ALLELE_FREQ\tTOTAL_GENOTYPE\tTOTAL_HET_GENOTYPE\tTOTAL_HOM_ALT_GENOTYPE\tTOTAL_HOM_REF_GENOTYPE\n";
+use Getopt::Long;
+my ($inpVCF, $outFile);
+GetOptions(
+	'vcf=s' => \$inpVCF,
+	'out=s' => \$outFile,
+) or die "Usage: $0 --vcf VCF_FILE --out OUTPUT_FILE\n";
+if (!defined $inpVCF | !defined $outFile) {
+	die "Please provide both the VCF and output files.\nUsage: $0 --vcf VCF_FILE --out OUTPUT_FILE\n";
+}
+open(VCF, "$inpVCF") or die "Unable to read the input file: $inpVCF\n";
+open(OUT, ">$outFile") or die "Unable to write to the output file: $outFile\n";
+print OUT "CHR\tPOS\tREF\tALT\tALT_ALLELE_FREQ\tMINOR_ALLELE\tMINOR_ALLELE_FREQ\tTOTAL_GENOTYPE\tTOTAL_HET_GENOTYPE\tTOTAL_HOM_ALT_GENOTYPE\tTOTAL_HOM_REF_GENOTYPE\n";
 while (my $row=<VCF>) {
 	chomp $row;
 	my $homRefGTCount=0;
@@ -31,7 +40,7 @@ while (my $row=<VCF>) {
 		if ($freq =~ m{0[/|]1|1[/|]0|0[/|]0|1[/|]1}) {
 			$totalGTcount++;
 		}
-		}
+	}
 	if ($totalGTcount> 0) {
 		my $refAllelefreq= ((2*$homRefGTCount)+$totalHetGTCount)/(2*$totalGTcount);
 		my $altAllelefreq= ((2*$homAltGTCount)+$totalHetGTCount)/(2*$totalGTcount);
@@ -42,6 +51,6 @@ while (my $row=<VCF>) {
 			$minorAlleleFreq = $altAllelefreq;
 			$minorAllele = $alt;
 		}
-		print "$chr\t$pos\t$ref\t$alt\t$altAllelefreq\t$minorAllele\t$minorAlleleFreq\t$totalGTcount\t$totalHetGTCount\t$homAltGTCount\t$homRefGTCount\n";
+		print OUT "$chr\t$pos\t$ref\t$alt\t$altAllelefreq\t$minorAllele\t$minorAlleleFreq\t$totalGTcount\t$totalHetGTCount\t$homAltGTCount\t$homRefGTCount\n";
 	}
 }
